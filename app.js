c4d41d3e861bc83403760e01e150a7e304a96a15
adeogunesther95-lab <adeogunesther95@gmail.com>
2025-10-31 15:21:37 +0000

    added real time based on location

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+// Los Angeles
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  // Lagos
+  let lagosElement = document.querySelector("#lagos");
+  let lagosDateElement = lagosElement.querySelector(".date");
+  let lagosTimeElement = lagosElement.querySelector(".time");
+
+  let lagosTime = moment().tz("Africa/Lagos");
+
+  lagosDateElement.innerHTML = lagosTime.format("MMMM Do YYYY");
+  lagosTimeElement.innerHTML = lagosTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+// Lagos
